17de005ae00cd94d8cf189c09f0bac0d33a35e65
ky0422 <ky0422a@gmail.com>
2024-10-12 23:07:21 +0900

    updated `tests`

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,29 +5,27 @@
 
 #define RUN_TEST(name)                        \
     do {                                      \
-        std::cout << "Running test: " #name  \
-                  << std::endl;              \
-        test_##name();                        \
-        std::cout << "Test " #name " passed." \
-                  << std::endl;              \
+        try { \
+            std::cout << "🔁 ----- Running test: " #name  \
+                      << std::endl;              \
+            test_##name();                        \
+            std::cout << "✅ ----- Test " #name " passed." \
+                      << std::endl;              \
+        } catch (const BytecodeError& e) { \
+            cerr << "❌ ByteCode error: " << e.what() << " on (ptr)" << e.where() << endl; \
+        } \
     } while (0)
 
 #define ASSERT_EQ(x, y)                         \
     do {                                        \
         if ((x) != (y)) {                      \
-            std::cerr << "Assertion failed: " \
-                      << #x << " == " << #y   \
-                      << ", at " << __FILE__   \
-                      << ":" << __LINE__      \
+            std::cerr << "❌ Assertion failed at " \
+                      << __FILE__ << ":" << __LINE__      \
                       << std::endl;            \
+            std::cerr << " => " << #x << " == " << #y \
+                      << std::endl;   \
+            std::cerr << " => " << (x) << " == " << (y) \
+                      << std::endl;   \
             std::exit(EXIT_FAILURE);           \
         }                                       \
     } while (0)
-
-TEST_CASE(case_example) {
-    ASSERT_EQ(1, 1);
-}
-
-void TESTS_RUN_ALL() {
-    RUN_TEST(case_example);
-}
