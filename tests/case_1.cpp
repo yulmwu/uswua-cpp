cb6365d52895286bf04f151dedde5270e0109c1c
ky0422 <ky0422a@gmail.com>
2024-10-12 23:07:12 +0900

    updated `case_1 test`

--- tests/case_1.cpp
+++ tests/case_1.cpp
@@ -1,8 +1,18 @@
-//
-//  case_1.cpp
-//  uswua-cpp
-//
-//  Created by Kim Jun Young on 10/12/24.
-//
+#include <iostream>
+#include "tests.cpp"
 
-#include <stdio.h>
+using namespace std;
+
+TEST_CASE(case_1) {
+    uint8_t raw_1 = 0x01;
+
+    auto opcode_1 = to_opcode(raw_1);
+    ASSERT_EQ(opcode_1, Opcode::PUSH);
+    ASSERT_EQ(static_cast<int>(opcode_1), 0x01);
+    
+    uint8_t raw_2 = 0x10;
+
+    auto opcode_2 = to_opcode(raw_2);
+    ASSERT_EQ(opcode_2, Opcode::ADD);
+    ASSERT_EQ(static_cast<int>(opcode_2), 0x10);
+}
