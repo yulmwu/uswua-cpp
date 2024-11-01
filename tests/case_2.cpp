98b1ea852b445a02a663be09a43af761a607d990
ky0422 <ky0422a@gmail.com>
2024-11-01 22:18:44 +0900

    updated test case

--- tests/case_2.cpp
+++ tests/case_2.cpp
@@ -9,6 +9,9 @@ TEST_CASE(case_2) {
         Op(to_opcode(0x01), 10)
     };
     
-    auto *vm = new Vm(tests);
+    Stack stack;
+    auto *vm = new Vm(tests, stack);
     vm->execute();
+
+    ASSERT_EQ(stack.values_[0], 10);
 }
