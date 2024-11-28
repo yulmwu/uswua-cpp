7a60a3968d8c03a575f1d816bfa2d1c85070284b
ky0422 <ky0422a@gmail.com>
2024-11-28 10:37:11 +0900

    updated tests

--- tests/case_2.cpp
+++ tests/case_2.cpp
@@ -11,12 +11,16 @@ using namespace std;
 TEST_CASE(case_2) {
     vector tests = {
         Op(to_opcode(0x01), 10),
+        Op(to_opcode(0x01), 20),
+        Op(to_opcode(0x04), std::nullopt),
         Op(to_opcode(0xFE), 0x05),
     };
     
     Stack stack;
     Vm vm = Vm(tests, stack);
     vm.execute();
+    vm.stackDump();
 
-    ASSERT_EQ(stack.values_[0], 10);
+    ASSERT_EQ(stack.values_[0], 20);
+    ASSERT_EQ(stack.values_[1], 10);
 }
