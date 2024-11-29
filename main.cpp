8091377bc89654e6ad82d6094856556a703ab354
ky0422 <ky0422a@gmail.com>
2024-11-29 13:06:04 +0900

    updated main

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "uswua-core/vm.hpp"
 #include "uswua-utils/b2i.hpp"
 
+#include "uswua-utils/b2i.hpp"
 #include "uswua-utils/parser.hpp"
 
 #include "tests/tests_all.cpp"
@@ -13,17 +14,7 @@
 using namespace std;
 
 int main() {
-    auto code = R"(
-    push 0x01
-    store 0x00
-)";
-    
-    Parser p = Parser(code);
-    auto instructions = p.parse();
-    
-    for (auto i : instructions) {
-//        cout << i << endl;
-    }
+    TESTS_RUN_ALL();
 
     return 0;
 }
---
+++ tests/case_parser.cpp
@@ -0,0 +1,8 @@
+//
+//  case_parser.cpp
+//  uswua-cpp
+//
+//  Created by Kim Jun Young on 11/29/24.
+//
+
+#include <stdio.h>
