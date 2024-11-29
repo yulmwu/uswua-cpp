bf47b58b258754e2ea9192352e2972b4f68ec10c
ky0422 <ky0422a@gmail.com>
2024-11-29 13:06:29 +0900

    updated tests

--- tests/tests_all.cpp
+++ tests/tests_all.cpp
@@ -4,10 +4,12 @@
 #include "case_2.cpp"
 #include "case_b2i.cpp"
 #include "case_sum.cpp"
+#include "case_parser.cpp"
 
 void TESTS_RUN_ALL() {
     RUN_TEST(case_1);
     RUN_TEST(case_2);
     RUN_TEST(case_b2i);
     RUN_TEST(case_sum);
+    RUN_TEST(case_parser);
 }
