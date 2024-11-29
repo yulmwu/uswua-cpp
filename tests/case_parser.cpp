4f753206bc353334071c52cbcd992e49fdf448c3
ky0422 <ky0422a@gmail.com>
2024-11-29 13:06:19 +0900

    updated parser

--- tests/case_parser.cpp
+++ tests/case_parser.cpp
@@ -1,8 +1,72 @@
-//
-//  case_parser.cpp
-//  uswua-cpp
-//
-//  Created by Kim Jun Young on 11/29/24.
-//
+#include <iostream>
+#include <vector>
 
-#include <stdio.h>
+#include "tests.cpp"
+#include "../uswua-core/opcode.hpp"
+#include "../uswua-core/vm.hpp"
+#include "../uswua-core/stack.hpp"
+
+using namespace std;
+
+TEST_CASE(case_parser) {
+    auto code = R"(
+PUSH 0x00
+STORE 0x00
+PUSH 0x01
+STORE 0x01
+PROC 12
+LOAD 0x00
+LOAD 0x01
+ADD
+STORE 0x0
+LOAD 0x01
+PUSH 0x01
+ADD
+STORE 0x01
+LOAD 0x01
+PUSH 0x0A
+GTE
+JIF 18
+JMP 5
+LOAD 0x00
+LOAD 0x01
+NOOP
+)";
+    
+    std::vector<unsigned char> tests = {
+        /* 00 */ 0x01, 0x00, // PUSH 0
+        /* 01 */ 0x02, 0x00, // STORE 0 (r)   r = 0
+        /* 02 */ 0x01, 0x01, // PUSH 1
+        /* 03 */ 0x02, 0x01, // STORE 1 (i)   i = 1
+        
+        /* 04 */ 0x28, 0x0C, // PROC 12       def f():
+        /* 05 */ 0x03, 0x00, // LOAD 0 (r)
+        /* 06 */ 0x03, 0x01, // LOAD 1 (i)
+        /* 07 */ 0x10, // ADD
+        /* 08 */ 0x02, 0x00, // STORE 0 (r)       r = r + i
+
+        /* 09 */ 0x03, 0x01, // LOAD 1 (i)
+        /* 10 */ 0x01, 0x01, // PUSH 1
+        /* 11 */ 0x10, // ADD
+        /* 12 */ 0x02, 0x01, // STORE 1           i = i + 1
+        /* 13 */ 0x03, 0x01, // LOAD 1
+        /* 14 */ 0x01, 0x0A, // PUSH 10
+        /* 15 */ 0x23, // GTE (>=)                if (10 >= i) { JMP 5 } else { JIF 18 }
+        /* 16 */ 0x2D, 0x12, // JIF 18,
+
+        /* 17 */ 0x2C, 0x05, // JMP 5
+
+        /* 18 */ 0x03, 0x00, // LOAD 0
+        /* 19 */ 0x03, 0x01, // LOAD 1
+        /* 18 */ 0x00 // NOOP
+    };
+    
+    auto instructions_a = b2i_from(tests);
+    Parser p = Parser(code);
+    Instructions instructions_b = p.parse();
+    
+    for (auto i = 0; i < instructions_a.size(); i++) {
+        ASSERT_EQ(instructions_a[i], instructions_b[i]);
+//        cout << instructions_a[i] << " | " << instructions_b[i] << endl;
+    }
+}
--- uswua-utils/parser.cpp
+++ uswua-utils/parser.cpp
@@ -2,7 +2,13 @@
 
 #include <iostream>
 #include <sstream>
+
 #include "../uswua-core/opcode.hpp"
+#include "../uswua-core/error.hpp"
+
+#define CASE_OPCODE(opcode_str, enum_value) \
+    if (opcode == opcode_str) { return Opcode::enum_value; }
+
 
 using namespace std;
 
@@ -24,6 +30,40 @@ string trim(string str) {
     return str;
 }
 
+Opcode to_opcode(string opcode, Pointer pointer = 0) {
+    transform(opcode.begin(), opcode.end(), opcode.begin(), ::tolower);
+    
+    CASE_OPCODE("noop", NOOP)
+    CASE_OPCODE("push", PUSH)
+    CASE_OPCODE("store", STORE)
+    CASE_OPCODE("load", LOAD)
+    CASE_OPCODE("swap", SWAP)
+    CASE_OPCODE("add", ADD)
+    CASE_OPCODE("sub", SUB)
+    CASE_OPCODE("mul", MUL)
+    CASE_OPCODE("div", DIV)
+    CASE_OPCODE("mod", MOD)
+    CASE_OPCODE("and", AND)
+    CASE_OPCODE("or", OR)
+    CASE_OPCODE("xor", XOR)
+    CASE_OPCODE("not", NOT)
+    CASE_OPCODE("lsf", LSF)
+    CASE_OPCODE("rsf", RSF)
+    CASE_OPCODE("eq", EQ)
+    CASE_OPCODE("gt", GT)
+    CASE_OPCODE("lt", LT)
+    CASE_OPCODE("gte", GTE)
+    CASE_OPCODE("lte", LTE)
+    CASE_OPCODE("proc", PROC)
+    CASE_OPCODE("call", CALL)
+    CASE_OPCODE("ret", RET)
+    CASE_OPCODE("jmp", JMP)
+    CASE_OPCODE("jif", JIF)
+    CASE_OPCODE("dbg", DBG)
+    CASE_OPCODE("exit", EXIT)
+
+    throw BytecodeError(BytecodeError::BytecodeErrorKind::InvalidOpcode, pointer);
+}
 
 Instructions Parser::parse() {
     Instructions instructions;
@@ -40,5 +80,31 @@ Instructions Parser::parse() {
 }
 
 Op Parser::parse_op(vector<string> op) {
-    return Op(Opcode::NOOP, nullopt);
+    auto _opcode = op[0];
+    transform(_opcode.begin(), _opcode.end(), _opcode.begin(), ::tolower);
+    
+    auto opcode = to_opcode(_opcode);
+    auto operand = op[1];
+    
+    if (opcode == Opcode::PUSH) {
+        if (operand.starts_with("0x")) {
+            return Op(opcode, (Value)stoi(operand, 0, 16));
+        } else {
+            return Op(opcode, (Value)stoi(operand));
+        }
+    } else if (opcode == Opcode::STORE
+               || opcode == Opcode::LOAD 
+               || opcode == Opcode::JMP
+               || opcode == Opcode::JIF
+               || opcode == Opcode::DBG
+               || opcode == Opcode::PROC
+               || opcode == Opcode::CALL) {
+        if (operand.starts_with("0x")) {
+            return Op(opcode, (Pointer)stoi(operand, 0, 16));
+        } else {
+            return Op(opcode, (Pointer)stoi(operand));
+        }
+    } else {
+        return Op(opcode, nullopt);
+    }
 }
