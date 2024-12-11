aeb2d7c4312dfae62907d39a32bfae2bbd4e6637
ky0422 <ky0422a@gmail.com>
2024-12-12 08:10:20 +0900

    updated Heap

--- uswua-core/heap.cpp
+++ uswua-core/heap.cpp
@@ -1,8 +1,33 @@
-//
-//  heap.cpp
-//  uswua-cpp
-//
-//  Created by Kim Jun Young on 12/12/24.
-//
-
 #include "heap.hpp"
+
+void Heap::insert(Pointer pointer, Value value) {
+    this->data.insert({ pointer, value });
+}
+
+void Heap::insert_or_assign(Pointer pointer, Value value) {
+    this->data.insert_or_assign(pointer, value);
+}
+
+void Heap::erase(Pointer pointer, Pointer ptr) {
+    if (this->data.find(pointer) != data.end()) {
+        this->data.erase(pointer);
+    } else {
+        throw BytecodeError(BytecodeError::BytecodeErrorKind::NotFound, ptr);
+    }
+}
+
+void Heap::assign(Pointer pointer, Value value, Pointer ptr) {
+    if (this->data.find(pointer) != data.end()) {
+        this->data[pointer] = value;
+    } else {
+        throw BytecodeError(BytecodeError::BytecodeErrorKind::NotFound, ptr);
+    }
+}
+
+Value Heap::get(Pointer pointer, Pointer ptr) {
+    if (this->data.find(pointer) != data.end()) {
+        return this->data[pointer];
+    } else {
+        throw BytecodeError(BytecodeError::BytecodeErrorKind::NotFound, ptr);
+    }
+}
--- uswua-core/heap.hpp
+++ uswua-core/heap.hpp
@@ -1,13 +1,22 @@
-//
-//  heap.hpp
-//  uswua-cpp
-//
-//  Created by Kim Jun Young on 12/12/24.
-//
-
 #ifndef heap_hpp
 #define heap_hpp
 
-#include <stdio.h>
+#include <map>
+#include "opcode.hpp"
+#include "error.hpp"
+
+class Heap {
+public:
+    std::map<Pointer, Value> data;
+
+    void insert(Pointer, Value);
+    void insert_or_assign(Pointer, Value);
+    void erase(Pointer, Pointer);
+    void assign(Pointer, Value, Pointer);
+    Value get(Pointer, Pointer);
+
+    Heap() = default;
+};
+
 
 #endif /* heap_hpp */
