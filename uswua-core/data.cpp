35c4b144bcd4bc47425d791ec52ef5c84767304d
ky0422 <ky0422a@gmail.com>
2024-12-26 10:16:37 +0900

    updated Examples

---
+++ uswua-core/data.cpp
@@ -0,0 +1,8 @@
+//
+//  data.cpp
+//  uswua-cpp
+//
+//  Created by Kim Jun Young on 12/26/24.
+//
+
+#include "data.hpp"
---
+++ uswua-core/data.hpp
@@ -0,0 +1,13 @@
+//
+//  data.hpp
+//  uswua-cpp
+//
+//  Created by Kim Jun Young on 12/26/24.
+//
+
+#ifndef data_hpp
+#define data_hpp
+
+#include <stdio.h>
+
+#endif /* data_hpp */
